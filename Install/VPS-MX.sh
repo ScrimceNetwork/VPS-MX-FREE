3c4c6a3ad51c2139e467f1192df1e002ff2bb524
AorsiniYT <albertorsini12@gmail.com>
2022-06-05 01:39:18 -0500

    update

--- Install/VPS-MX.sh
+++ Install/VPS-MX.sh
@@ -371,6 +371,7 @@ cd $HOME
 msg -ne "Files: "
 wget -O $HOME/lista-arq ${REQUEST}/lista-arq > /dev/null 2>&1 && echo -e "\033[1;32m Verificado" || {
    echo -e "\033[1;32m Verificada"
+   invalid_key
    exit
    }
 sleep 1s
